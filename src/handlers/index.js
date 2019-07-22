cdd68ee94aea8a6475ae51663000825660c11246
markhorsman <mark.horsman@tam-one.nl>
2019-07-22 15:27:43 +0200

    Added handler for stock item update

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,4 +2,5 @@ module.exports  = {
     error : require('./error'),
     customerContact : require('./customerContact'),
     contItem: require('./contItem'),
+    stock: require('./stock'),
 };
--- src/handlers/stock.js
+++ src/handlers/stock.js
@@ -3,11 +3,12 @@ const sql = P.promisifyAll(require("mssql"));
 const errors = require('../../lib/classes/errors');
 
 module.exports = function (req, res, next) {
-    const update = (recid, status) => {
+    const update = (recid, status, quantity, type) => {
         return req.mssql.request()
             .input('status', sql.Int, parseInt(status))
             .input('recid', sql.NVarChar, recid)
-            .query('UPDATE dbo.ContItems SET STATUS = @status WHERE RECID = @recid')
+            .input('qty', sql.Int, quantity)
+            .query(`UPDATE dbo.Stock SET STATUS = @status, STKLEVEL = STKLEVEL ${type === 'add' ? '+' : '-'} @qty WHERE RECID = @recid`)
             .then(rtn)
             .catch(next)
             ;
@@ -15,10 +16,10 @@ module.exports = function (req, res, next) {
 
     const rtn = result => {
         if (!result || !result.rowsAffected[0])
-            throw new errors.http.NotFound(`Updaten van contitem met recid: ${req.params.recid} is mislukt.`);
+            throw new errors.http.NotFound(`Updaten van stock item met recid: ${req.params.recid} is mislukt.`);
 
         return res.status(200).send({ success: true, data: req.params });
     };
 
-    return update(req.params.recid, req.params.status);
+    return update(req.params.recid, req.params.status, req.params.quantity, req.params.type);
 }
